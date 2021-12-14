da323e4257b6b52cd56afafa88ea14b69a568b38
vanlgo <goellevan@gmail.com>
2021-12-14 01:22:03 +0000

    adding POST method to tweeter form

--- server/index.js
+++ server/index.js
@@ -32,6 +32,10 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
+app.post("/tweets", (req, res) => {
+res.redirect("/");
+})
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
