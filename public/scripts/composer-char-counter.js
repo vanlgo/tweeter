fe886cbd2889ed87c550132d5e662e86ea075710
vanlgo <goellevan@gmail.com>
2021-12-15 02:13:44 +0000

    fixed counter css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,15 +2,20 @@ console.log("Is this working?")
 
 $(document).ready(function() {
 
-  $("#tweet-text").on("input", function () {
+  $("#tweet-text").on("input", function() {
+    // checking how many characters are left in the counter
     let charRemain = 140 - $(this).val().length;
 
-    $(".counter").text(charRemain);
+    // searching for counter in the DOM tree
+    let counter = $(this).next("div").children(".counter");
+
+    // allowing counter to find remaining count
+    counter.text(charRemain);
 
     if (charRemain < 0) {
-      $(".counter").addClass("err")
+      counter.addClass("charError")
     } else {
-      $(".counter").removeClass("err")
+      counter.removeClass("charError")
     }
   });
 });
